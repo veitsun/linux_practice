3aba4a5b80839bfa2f2d7497a6b85c1b351ea23a
sunwei <sw20002000@163.com>
2025-01-14 11:07:55 +0800

    libunp

--- echo_cliserver/tcpserver01.cpp
+++ echo_cliserver/tcpserver01.cpp
@@ -1,11 +1,36 @@
+extern "C" {
 #include "unp.h"
+}
 #include <cstdio>
-#include <iostream>
+#include <strings.h>
+#include <sys/socket.h>
 
 // using namespace std;
+int main(int argc, char *argv[]) {
+  int listenfd, connfd;
+  pid_t childpid;
+  socklen_t clilen;
+  struct sockaddr_in cliaddr, servaddr;
 
-int main() {
-  printf("jjjj");
-  std::cout << "aaa" << std::endl;
-  return 0;
+  listenfd = Socket(AF_INET, SOCK_STREAM, 0);
+  bzero(&servaddr, sizeof(servaddr));
+  servaddr.sin_family = AF_INET;
+  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  servaddr.sin_port = htons(SERV_PORT);
+
+  Bind(listenfd, (SA *)&servaddr, sizeof(servaddr));
+  listen(listenfd, LISTENQ);
+  // 服务器阻塞于 accept 调用，等待客户连接的完成
+
+  for (;;) {
+    clilen = sizeof(cliaddr);
+    connfd = Accept(listenfd, (SA *)&cliaddr, &clilen);
+
+    if ((childpid = Fork()) == 0) {
+      Close(listenfd);
+      str_echo(connfd);
+      exit(0);
+    }
+    Close(0);
+  }
 }
